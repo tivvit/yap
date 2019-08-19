b0f6fcc5f33db9f455fac25c59142be1fe14fe37
Vít Listík <tivvit@seznam.cz>
2019-08-20 00:18:27 +0200

    Support for directory changes (#23)
    * added support for directory states
    * Closes #4 
    * improved file state checking

---
+++ pkg/state/dirState.go
@@ -0,0 +1,26 @@
+package state
+
+import (
+	"encoding/json"
+	"time"
+)
+
+type DirState struct {
+	Exists   bool                 `json:"exists,omitempty"`
+	Files    []string             `json:"files,omitempty"`
+	ModTimes []time.Time `json:"mod-times,omitempty"`
+	Md5s     []string    `json:"md5s,omitempty"`
+}
+
+func (ds DirState) Serialize() (string, error) {
+	b, err := json.Marshal(ds)
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
+}
+
+func (ds *DirState) Deserialize(s string) error {
+	err := json.Unmarshal([]byte(s), &ds)
+	return err
+}
--- pkg/structs/file.go
+++ pkg/structs/file.go
@@ -2,10 +2,13 @@ package structs
 
 import (
 	"github.com/emicklei/dot"
+	"github.com/tivvit/yap/pkg/state"
 	"github.com/tivvit/yap/pkg/stateStorage"
 	"github.com/tivvit/yap/pkg/utils"
 	"log"
 	"os"
+	"path/filepath"
+	"reflect"
 	"time"
 )
 
@@ -23,38 +26,170 @@ type File struct {
 	Mode     os.FileMode
 	ModTime  time.Time
 	IsDir    bool
+	Exists   bool
 }
 
 func (f File) GetState() (string, error) {
+	f.Analyze()
+	if f.IsDir {
+		files, err := f.getDirList()
+		if err != nil {
+			return "", err
+		}
+		return state.DirState{
+			Exists: f.Exists,
+			Files: files,
+			ModTimes: modTimes(files),
+			Md5s: md5s(files),
+		}.Serialize()
+	} else {
+		md5, err := f.getFileMd5()
+		if err != nil {
+			return "", err
+		}
+		return state.FileState{
+			Exists: f.Exists,
+			ModTime: f.ModTime,
+			Md5: md5,
+		}.Serialize()
+	}
+}
+
+func (f File) getFileMd5() (string, error) {
 	r, err := os.Open(f.Name)
 	if err != nil {
 		return "", err
 	}
-
 	defer func() {
 		err := r.Close()
 		if err != nil {
 			log.Println(err)
 		}
 	}()
-
 	return utils.Md5Checksum(r)
 }
 
-func (f File) Changed(state stateStorage.State, p *Pipeline) bool {
-	md5Sum, err := f.GetState()
+func (f File) getDirList() ([]string, error) {
+	var files []string
+	err := filepath.Walk(f.Name, func(path string, info os.FileInfo, err error) error {
+		if !info.IsDir() {
+			files = append(files, path)
+		}
+		return nil
+	})
+	return files, err
+}
+
+func modTimes(files []string) []time.Time {
+	var modTimes []time.Time
+	for _, f := range files {
+		fo := File{
+			Name: f,
+		}
+		fo.Analyze()
+		modTimes = append(modTimes, fo.ModTime)
+	}
+	return modTimes
+}
+
+func md5s(files []string) []string {
+	var md5s []string
+	for _, f := range files {
+		fo := File{
+			Name: f,
+		}
+		md5, err := fo.getFileMd5()
+		if err != nil {
+			log.Println(err)
+		}
+		md5s = append(md5s, md5)
+	}
+	return md5s
+}
+
+func loadState(s string) (interface{}, error) {
+	// we do not know if stored type is dir or file
+	fs := state.FileState{}
+	err := fs.Deserialize(s)
 	if err != nil {
+		// it is not fileState = trying DirState
+	} else {
+		return fs, nil
+	}
+	ds := state.DirState{}
+	err = ds.Deserialize(s)
+	if err != nil {
+		return nil, err
+	}
+	return ds, nil
+}
+
+func (f File) Changed(s stateStorage.State, p *Pipeline) bool {
+	// todo this should compare with some exact state not with my state
+	oldState, err := loadState(s.Get(f.GetFullName()))
+	if err != nil {
+		// not possible to read state = recompute
 		return true
 	}
-	s := state.Get(f.Name)
-	if s != md5Sum {
+	f.Analyze()
+	switch oldState.(type) {
+	case state.FileState:
+		oldFS := oldState.(state.FileState)
+		if f.Exists != oldFS.Exists {
+			return true
+		}
+		if f.Analyzed && f.IsDir {
+			return true
+		}
+		if f.ModTime != oldFS.ModTime {
+			return true
+		}
+		md5, err := f.getFileMd5()
+		if err != nil {
+			log.Println(err)
+			return true
+		}
+		if md5 != oldFS.Md5 {
+			return true
+		}
+		return false
+	case state.DirState:
+		oldDS := oldState.(state.DirState)
+		if f.Exists != oldDS.Exists {
+			return true
+		}
+		if f.Analyzed && !f.IsDir {
+			return true
+		}
+		dirList, err := f.getDirList()
+		if err != nil {
+			log.Println(err)
+			return true
+		}
+		if !reflect.DeepEqual(dirList, oldDS.Files) {
+			return true
+		}
+		modTimes := modTimes(dirList)
+		if !reflect.DeepEqual(modTimes, oldDS.ModTimes) {
+			return true
+		}
+		md5s := md5s(dirList)
+		if !reflect.DeepEqual(md5s, oldDS.Md5s) {
+			return true
+		}
+		return false
+	default:
 		return true
 	}
-	return false
 }
 
 func (f *File) Analyze() {
 	fileInfo, err := os.Stat(f.Name)
+	if os.IsNotExist(err) {
+		f.Exists = false
+	} else {
+		f.Exists = true
+	}
 	if err != nil {
 		f.Analyzed = false
 		log.Println(err)
@@ -81,8 +216,8 @@ func (f File) GetFullName() string {
 
 func (f File) Visualize(ctx *dot.Graph, fileMap *map[string]*File, m *map[string]dot.Node, conf VisualizeConf) {
 	if f.Analyzed && f.IsDir {
-		(*m)[f.GetFullName()] = ctx.Node(DotFilePrefix+ f.Name).Attr("shape", DirShape).Label(f.Name)
+		(*m)[f.GetFullName()] = ctx.Node(DotFilePrefix+f.Name).Attr("shape", DirShape).Label(f.Name)
 	} else {
-		(*m)[f.GetFullName()] = ctx.Node(DotFilePrefix+ f.Name).Attr("shape", FileShape).Label(f.Name)
+		(*m)[f.GetFullName()] = ctx.Node(DotFilePrefix+f.Name).Attr("shape", FileShape).Label(f.Name)
 	}
 }
