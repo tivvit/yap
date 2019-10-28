edc7607343db32e978a8342955986c8dd86827f5
Vit Listik <tivvit@seznam.cz>
2019-10-28 23:49:34 +0100

    added tracker tests

--- pkg/tracker/tracker_test.go
+++ pkg/tracker/tracker_test.go
@@ -20,10 +20,49 @@ func TestTrack(t *testing.T) {
 	}
 }
 
+func TestMultiTrack(t *testing.T) {
+	tr := NewTracker()
+	d := 5 * time.Millisecond
+	tr.Start("a")
+	tr.Start("b")
+	time.Sleep(d)
+	mda, err := tr.Stop("a")
+	if err != nil {
+		t.Fail()
+	}
+	time.Sleep(d)
+	mdb, err := tr.Stop("b")
+	if err != nil {
+		t.Fail()
+	}
+	mdat := mda.Truncate(time.Millisecond)
+	mdbt := mdb.Truncate(time.Millisecond)
+	if mdat != d {
+		t.Fail()
+	}
+	if mdbt != 2 * d {
+		t.Fail()
+	}
+}
+
 
 func TestMissing(t *testing.T) {
 	tr := NewTracker()
 	_, err := tr.Stop("a")
+	if err == nil {
+		t.Fail()
+	}
+}
+
+
+func TestMultiRead(t *testing.T) {
+	tr := NewTracker()
+	tr.Start("a")
+	_, err := tr.Stop("a")
+	if err != nil {
+		t.Fail()
+	}
+	_, err = tr.Stop("a")
 	if err == nil {
 		t.Fail()
 	}
