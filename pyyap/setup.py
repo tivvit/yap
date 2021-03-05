03b167d748a2694ebe95e4bfe3c60a988aba3622
Vit Listik <tivvit@seznam.cz>
2021-03-05 22:29:26 +0100

    python package

---
+++ pyyap/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='yap',
+    version='1.0.0',
+    author='tivvit',
+    author_email='tivvit@seznam.cz',
+    packages=[
+        "yap"
+    ],
+    url='https://github.com/tivvit/yap',
+    install_requires=[
+        "pyyaml",
+    ],
+)
---
+++ pyyap/yap/__init__.py
@@ -0,0 +1,4 @@
+from .yap import Block
+from .yap import VERSION
+from .yap import DictBlock
+from .yap import Pipeline
